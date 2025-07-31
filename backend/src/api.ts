b4d896e6708acae320a7ec2626f44ec771531f72
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-07-31 08:05:34 +0300

    feat(backend): add the no pagination endpoint
    - create the endpoint to get all the users with no pagination
      whatsoever.
    - also realised why I was not using rows[0] while destructuring the
      users array. A stored procedure will retunr an array of items, whereas
      a direct query will retunr the exact values with no need for
      destructuring. Recal this when you switch back to using stored
      procedures
    
    NOTE: need to make the pages dynamic now, not static

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -4,6 +4,38 @@ import { pool } from "./db";
 
 const mainRouter = Router();
 
+//get users with no pagination
+mainRouter.get(
+  "/no-pagination",
+  async (request: Request, response: Response) => {
+    try {
+      const connection = await pool.getConnection();
+      const [rows]: any = await connection.execute(`SELECT * FROM users;`);
+      // NOTE: you will not destructure only the first element of the array
+      // i.e rows[0] but instead you capture the entire thing. If you do the
+      // former, only one value is returned. Why?
+      // Stored Procedure retunrn the array. Actual queries return the values directy
+      const users = rows as IUsers[];
+
+      return response.status(200).json({
+        code: 200,
+        status: "success",
+        message: "Users succesfully retrieved",
+        data: { users },
+        metadata: null,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        code: 500,
+        status: "error",
+        message: "Internal Server error",
+        data: { error },
+        metadata: null,
+      });
+    }
+  },
+);
+
 //get users with limit-offset method
 mainRouter.get(
   "/limit-offset",
@@ -15,7 +47,8 @@ mainRouter.get(
       );
       // NOTE: you will not destructure only the first element of the array
       // i.e rows[0] but instead you capture the entire thing. If you do the
-      // former, only one value is returned. Learn why?
+      // former, only one value is returned. Why?
+      // Stored Procedure retunrn the array. Actual queries return the values directy
       const users = rows as IUsers;
 
       return response.status(200).json({
@@ -38,42 +71,35 @@ mainRouter.get(
 );
 
 //get users with cursor method
-mainRouter.get(
-  "/cursor",
-  async (request: Request, response: Response) => {
-    try {
-      const connection = await pool.getConnection();
-      await connection.execute(`SET @user_id=7;`);
-      const [rows]: any = await connection.query(
-        `SELECT * FROM users WHERE id>@user_id LIMIT 10; `,
-      );
-      // NOTE: Also here you will not destructure only the first element of the array
-      // i.e rows[0] but instead you capture the entire thing. If you do the
-      // former, only one value is returned. Learn why?
-      const users = rows as IUsers;
+mainRouter.get("/cursor", async (request: Request, response: Response) => {
+  try {
+    const connection = await pool.getConnection();
+    await connection.execute(`SET @user_id=7;`);
+    const [rows]: any = await connection.query(
+      `SELECT * FROM users WHERE id>@user_id LIMIT 10; `,
+    );
+    // NOTE: you will not destructure only the first element of the array
+    // i.e rows[0] but instead you capture the entire thing. If you do the
+    // former, only one value is returned. Why?
+    // Stored Procedure retunrn the array. Actual queries return the values directy
+    const users = rows as IUsers;
 
-      return response.status(200).json({
-        code: 200,
-        status: "success",
-        message: "Users succesfully retrieved",
-        data: { users },
-        metadata: null,
-      });
-    } catch (error) {
-      return response.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Internal Server error",
-        data: { error },
-        metadata: null,
-      });
-    }
-  },
-);
-
-// NOTE: my preferred way, but too verbose here
-//router.get( "/cursor-users",
-//  async function getUsersWithCursor(request: Request, response: Response) {},
-//);
+    return response.status(200).json({
+      code: 200,
+      status: "success",
+      message: "Users succesfully retrieved",
+      data: { users },
+      metadata: null,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      code: 500,
+      status: "error",
+      message: "Internal Server error",
+      data: { error },
+      metadata: null,
+    });
+  }
+});
 
 export default mainRouter;
