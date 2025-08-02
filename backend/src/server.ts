b34b062ef3918a0cb57f7632fe015857e8018109
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-08-02 16:33:11 +0300

    feat(backend): implement limit-offset dynamically
    - create the logic to implemt pagination with limit offset dynamically,
      you do not have to set the numbers manully and the resposne object is
      well detailed to let you know which page youre on.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express, { json } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import mainRouter from "./api";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(json());
+app.use(cors());
 app.use(mainRouter);
 
 app.listen(port, () => {
